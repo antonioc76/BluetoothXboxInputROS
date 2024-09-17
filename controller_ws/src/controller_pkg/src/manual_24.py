7cb1e2469b64cb85d8b18a92bccbcf9c7486debc
antonioc76 <antoniochappell@protonmail.com>
2024-09-17 18:16:12 -0500

    just making manual node

---
+++ controller_ws/src/controller_pkg/src/manual_24.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python3
+
+import rclpy
+from rclpy.node import Node
+from autonav_msgs.msg import ControllerInput
+
+#!/usr/bin/env python3
+
+import rclpy
+import numpy as np
+from rclpy.node import Node
+from autonav_msgs.msg import ControllerInput
+
+class Manual24(Node):
+
+    def __init__(self):
+        super().__init__('manual_24')
+
+        self.max_forward_speed = 1
+        self.max_angular_speed = np.pi/4
+
+        self.controller_state = {}
+
+        self.subscription = self.create_subscription(
+            ControllerInput,
+            'autonav/controller_input',
+            self.input_callback,
+            10)
+        
+        self.subscription  # prevent unused variable warning
+
+
+    def input_callback(self, msg):
+        self.deserialize_controller_state(msg)
+        self.get_logger().info(f"I heard: {str(self.controller_state)}")
+
+        self.compose_motorinput_message()
+
+
+    def deserialize_controller_state(self, msg):
+        attributes = [n for n in dir(msg) if not (n.startswith('__') or n.startswith('_'))]
+        attributes.remove('SLOT_TYPES')
+        attributes.remove('get_fields_and_field_types')
+        for attribute in attributes:
+            self.controller_state[attribute] = getattr(msg, attribute)
+
+
+    def normalize(self, x, min, max, lower_bound):
+        return ((-lower_bound + 1) * (x - min) / (max - min) ) + lower_bound
+    
+    def compose_motorinput_message(self):
+        pass
+
+
+def main(args=None):
+    rclpy.init(args=args)
+
+    controller_listener = Manual24()
+
+    rclpy.spin(controller_listener)
+
+    # Destroy the node explicitly
+    # (optional - otherwise it will be done automatically
+    # when the garbage collector destroys the node object)
+    controller_listener.destroy_node()
+    rclpy.shutdown()
+
+
+if __name__ == '__main__':
+    main()
