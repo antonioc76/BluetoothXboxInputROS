1a471e3a8427701e71f371ac54358c6892cf1b55
antonioc76 <antoniochappell@protonmail.com>
2024-09-17 20:17:47 -0500

    working on manual control

--- controller_ws/src/controller_pkg/src/manual_24.py
+++ controller_ws/src/controller_pkg/src/manual_24.py
@@ -2,7 +2,7 @@
 
 import rclpy
 from rclpy.node import Node
-from autonav_msgs.msg import ControllerInput
+from autonav_msgs.msg import ControllerInput, MotorInput
 
 #!/usr/bin/env python3
 
@@ -21,13 +21,19 @@ class Manual24(Node):
 
         self.controller_state = {}
 
-        self.subscription = self.create_subscription(
+        self.controllerSubscriber = self.create_subscription(
             ControllerInput,
             'autonav/controller_input',
             self.input_callback,
             10)
         
-        self.subscription  # prevent unused variable warning
+        self.motorPublisher = self.create_publisher(
+            MotorInput,
+            'autonav/MotorInput',
+            10
+        )
+
+        self.controllerSubscriber  # prevent unused variable warning
 
 
     def input_callback(self, msg):
@@ -45,11 +51,19 @@ class Manual24(Node):
             self.controller_state[attribute] = getattr(msg, attribute)
 
 
-    def normalize(self, x, min, max, lower_bound):
-        return ((-lower_bound + 1) * (x - min) / (max - min) ) + lower_bound
+    def normalize(self, input, output_start, output_end, input_start, input_end):
+        return output_start + ((output_end - output_start) / (input_end - input_start)) * (input - input_start)
     
+
     def compose_motorinput_message(self):
-        pass
+        forward_velocity = self.normalize(self.controller_state["abs_gas"] - self.controller_state["abs_brake"], -self.max_forward_speed, self.max_forward_speed, -1, 1)
+        angular_velocity = self.normalize(self.controller_state["abs_x"], -self.max_angular_speed, self.max_angular_speed, -1, 1)
+
+        motor_msg = MotorInput()
+        motor_msg.forward_velocity = forward_velocity
+        motor_msg.angular_velocity = angular_velocity
+
+        self.motorPublisher.publish(motor_msg)
 
 
 def main(args=None):
